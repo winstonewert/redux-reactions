94135f8512e3863f8ba32e3f4a9a89ae39f7d9ee
Winston Ewert <WinstonEwert@gmail.com>
2016-01-11 13:49:45 -0800

    Fixes #6
    Removed obsolete CALL_API symbol.

--- examples/real-world/reactions/api.js
+++ examples/real-world/reactions/api.js
@@ -71,11 +71,6 @@ export const Schemas = {
   REPO_ARRAY: arrayOf(repoSchema)
 }
 
-// Action key that carries API call info interpreted by this Redux middleware.
-export const CALL_API = Symbol('Call API')
-
-// A Redux middleware that interprets actions with CALL_API info specified.
-// Performs the call and promises when such actions are dispatched.
 export default fromPromiseFactory((reaction) => {
   let { endpoint } = reaction 
   const { schema } = reaction 
